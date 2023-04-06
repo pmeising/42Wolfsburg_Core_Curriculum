baf98e0c5ce014f9b33d6849326f321ddf4bb4c0
pmeising <pmeising@student.42wolfsburg.de>
2023-04-06 21:33:55 +0200

    input checks written. Implemented.

--- Level_5/C++9/ex01/RPN.cpp
+++ Level_5/C++9/ex01/RPN.cpp
@@ -6,7 +6,7 @@
 /*   By: pmeising <pmeising@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/06 20:51:16 by pmeising          #+#    #+#             */
-/*   Updated: 2023/04/06 21:27:24 by pmeising         ###   ########.fr       */
+/*   Updated: 2023/04/06 21:33:31 by pmeising         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,13 +49,25 @@ int	RPN::inputCheck(const std::string input)
 	for (int i = 0; i < size; i++)
 		if ((input[i] >= '0' && input[i] <= '9') || input[i] == '+' || input[i] == '-' || input[i] == '*' || input[i] == '/')
 			if (i < (size -1) && input[i + 1] != ' ')
+			{
 				std::cout << "Error\n";
+				return (-1);
+			}
 		else if (input[i] != ' ')
+		{
 			std::cout << "Error\n";
+			return (-1);
+		}
 	if (input.empty())
+	{
 		std::cout << "Error\n";
+		return (-1);
+	}
 	else if (input.find("(") != std::string::npos || input.find(")") != std::string::npos) // If bracket is found
+	{
 		std::cout << "Error\n";
+		return (-1);
+	}
 	int count_a = 0;
 	int count_b = 0;
 	for (int i = 0; i < size; i++)
@@ -64,7 +76,15 @@ int	RPN::inputCheck(const std::string input)
 			count_a++;
 		else if (input[i] == '+' || input[i] == '-' || input[i] == '*' || input[i] == '/')
 			count_b++;
-		if (count_b >= count_a)
+		if (count_b > count_a && count_a < 2)
+		{
 			std::cout << "Error\n";
+			return (-1);
+		}
+	}
+	if (count_b >= count_a)
+	{
+		std::cout << "Error\n";
+		return (-1);
 	}
 }
