e293dced420b646d6680ced8529d0a9193417ef2
pmeising <pmeising@student.42wolfsburg.de>
2023-04-07 18:36:49 +0200

    Added rudimentary class for PmergeMe

--- Level_5/C++9/ex02/PmergeMe.hpp
+++ Level_5/C++9/ex02/PmergeMe.hpp
@@ -1,1 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PmergeMe.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pmeising <pmeising@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/07 18:33:19 by pmeising          #+#    #+#             */
+/*   Updated: 2023/04/07 18:35:48 by pmeising         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
+#ifndef PMERGEME
+# define PMERGEME
+# include <string>
+# include <iostream>
+# include <vector>
+# include <list>
+# define DEFAULT "\033[0;39m"
+# define B_RED "\033[1;31m"
+# define B_BLUE "\033[1;94m"
+# define B_YELLOW "\033[1;33m"
+# define B_GREEN "\033[1;32m"
+# define B_PINK "\033[1;35m"
+
+class PmergeMe
+{
+	private:
+		/* data */
+	public:
+		PmergeMe(/* args */);
+		~PmergeMe();
+		void	sort(const std::string& sequence);
+};
+
+
+
+#endif
--- Level_5/C++9/ex02/tests.cpp
+++ Level_5/C++9/ex02/tests.cpp
@@ -5,28 +5,27 @@ int	main()
 	PmergeMe	obj;
 
 	std::cout << B_BLUE << "--------------- TEST ONE: ERROR HANDLING ----------------------\n\n" << DEFAULT;
-	RPN	rpn;
 	std::cout << "Empty input. Expected Error." << std::endl;
-	rpn.calculate("");
+	obj.sort("");
 	std::cout << "Operator in beginning. Expected Error." << std::endl;
-	rpn.calculate("-");
+	obj.sort("-");
 	std::cout << "Operator on 2nd. pos. Expected Error." << std::endl;
-	rpn.calculate("2 *");
+	obj.sort("2 *");
 	std::cout << "Imbalance. Expected Error." << std::endl;
-	rpn.calculate("2 2 - - +");
+	obj.sort("2 2 - - +");
 	std::cout << "Brackets found. Expected Error." << std::endl;
-	rpn.calculate("2 9 * 2 (2) - -");
+	obj.sort("2 9 * 2 (2) - -");
 	std::cout << "operand over 10. Expected Error." << std::endl;
-	rpn.calculate("5 1 20 4 - + +");
+	obj.sort("5 1 20 4 - + +");
 
 	std::cout << B_BLUE << "\n----------------------- TEST TWO: CALCULATIONS ------------------ \n\n" << DEFAULT;
 	std::cout << "2.1 Expected: 8\n";
-	rpn.calculate("2 3 2 * +");
+	obj.sort("2 3 2 * +");
 	std::cout << "2.2 Expected: 4\n";
-	rpn.calculate("5 1 2 4 - + +");
+	obj.sort("5 1 2 4 - + +");
 	std::cout << "2.3 Expected: 194\n";
-	rpn.calculate("2 3 + 9 * 4 * 1 / 5 + 9 +");
+	obj.sort("2 3 + 9 * 4 * 1 / 5 + 9 +");
 	std::cout << "2.4 Expected: 23\n";
-	rpn.calculate("3 2 + 2 4 5 1 2 - + * + +");
+	obj.sort("3 2 + 2 4 5 1 2 - + * + +");
 	return (0);
 }
